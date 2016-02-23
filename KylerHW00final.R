734843ab477a8b847ff39e3825f4388bfe661d39
Kyler Blodgett <kylerblodgett@kylers-mbp.middlebury.edu>
2016-02-22 23:39:34 -0500

    Changes the name, date, and URL. Hope this works! - Kyler

---
+++ KylerHW00final.R
@@ -0,0 +1,14 @@
+---
+  title: "Testing"
+author: "Kyler Blodgett"
+date: "February 24, 2016"
+output: html_document
+---
+  
+  Your tasks!
+  
+  * Change the name of the author above to yours
+* Change the date
+* Replace the NHL URL with the URL for the document you just posted to Rpubs **here**: [My RPubs Document](http://rpubs.com/KylerBlodgett/hw00KB)
+* Click Knit HTML
+* Please indicate how much time you spent on this assignment: 40 minutes
